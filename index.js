70bd92608541c2eeec66db5910c6512ccf948cab
zhenllyalvarez <dev.zhenllyalvarez@gmail.com>
2023-11-26 21:01:43 +0800

    updated index.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // index.js
-console.log('Hello, World!');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, Node.js!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
